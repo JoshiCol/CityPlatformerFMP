30f781386a33b84f7e035be649f4cc2a62b9e217
JoshiCol <BAR23001353@HeartofYorkshire.ac.uk>
2025-03-19 13:12:58 +0000

    Big Changes to Prototype
    Assets for the new map, character controller.

--- City Platformer FMP/Assets/FreeLookCamera.cs
+++ City Platformer FMP/Assets/FreeLookCamera.cs
@@ -11,7 +11,10 @@ public class FreeLookCamera : MonoBehaviour
 
     [Header("Movement")]
     [SerializeField] private float movementSensitivity = 15f;
+    [SerializeField] private float sprintMultiplier = 2f;
+    [SerializeField] private float slowMultiplier = 0.5f;
     [SerializeField] private float upDownSensitivity = 10f;
+    private float movementMultiplier;
 
     // Start is called before the first frame update
     void Start()
@@ -28,11 +31,23 @@ public class FreeLookCamera : MonoBehaviour
 
     void MoveCam()
     {
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            movementMultiplier = sprintMultiplier;
+        }
+        else if(Input.GetKey(KeyCode.LeftAlt))
+        {
+            movementMultiplier = slowMultiplier;
+        }
+        else
+        {
+            movementMultiplier = 1;
+        }
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-        float moveUp = Input.GetKey(KeyCode.LeftShift) ? -1 : Input.GetKey(KeyCode.Space) ? 1 : 0;
+        float moveUp = Input.GetKey(KeyCode.LeftControl) ? -1 : Input.GetKey(KeyCode.Space) ? 1 : 0;
 
-        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical) * movementSensitivity;
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical) * movementSensitivity * movementMultiplier;
         transform.Translate(movement * Time.deltaTime);
         transform.position = new Vector3(transform.position.x, transform.position.y + (moveUp * upDownSensitivity * Time.deltaTime), transform.position.z);
     }
